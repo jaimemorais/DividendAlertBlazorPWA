44efa325a2dd1eefaa9aac915b16bbaaf529856b
Jaime Morais <jaimemorais@gmail.com>
2019-12-14 12:45:02 -0300

    Add my stock list page

---
+++ DividendAlertBlazorPWA/Model/Stock.cs
@@ -0,0 +1,10 @@
+﻿namespace DividendAlertBlazorPWA.Model
+{
+    public class Stock
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+
+        public bool Selected { get; set; }
+    }
+}
